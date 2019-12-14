2e8c81f302d47305a4d7d027e8065b9ee3d7c738
Sarah Roberts <sarahr@cyverse.org>
2019-12-13 20:08:40 -0700

    CORE-675: Created a logging shim for use with Echo and Logrus

--- main.go
+++ main.go
@@ -5,10 +5,32 @@ import (
 	"github.com/cyverse-de/requests/api"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/sirupsen/logrus"
 )
+
+// buildLoggerEntry sets some logging options then returns a logger entry with some custom fields
+// for convenience.
+func buildLoggerEntry() *logrus.Entry {
+
+	// Enable logging the file name and line number.
+	logrus.SetReportCaller(true)
+
+	// Set the logging format to JSON for now because that's what Echo's middleware uses.
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	// Return the custom log entry.
+	return logrus.WithFields(logrus.Fields{
+		"service": "app-exposer",
+		"art-id":  "app-exposer",
+		"group":   "org.cyverse",
+	})
+}
 
 func main() {
 	e := echo.New()
+
+	// Set a custom logger.
+	e.Logger = Logger{Entry: buildLoggerEntry()}
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
