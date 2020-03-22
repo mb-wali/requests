7409bceb0ddb87de7697ca327790ef3e8f5d28e7
Sarah Roberts <sarahr@cyverse.org>
2020-03-21 23:17:43 -0700

    CORE-675: added the initial implementation of a client library for iplant-email

---
+++ clients/iplantemail/main.go
@@ -0,0 +1,61 @@
+package iplantemail
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+// EmailRequestBody represents a request body sent to iplant-email.
+type EmailRequestBody struct {
+	To       string      `json:"to"`
+	Template string      `json:"template"`
+	Subject  string      `json:"subject"`
+	Values   interface{} `json:"values"`
+}
+
+// Client describes a single instance of this client library.
+type Client struct {
+	baseURL string
+}
+
+// NewClient creates a new instance of this client library.
+func NewClient(baseURL string) *Client {
+	return &Client{baseURL: baseURL}
+}
+
+// SendRequestSubmittedEmail sends an email corresponding to a request.
+func (c *Client) SendRequestSubmittedEmail(emailAddress, templateName string, requestDetails interface{}) error {
+	errorMessage := "unable to send reqest notification email"
+	var err error
+
+	// Build the request body.
+	body, err := json.Marshal(&EmailRequestBody{
+		To:       emailAddress,
+		Template: templateName,
+		Subject:  "New Administrative Request",
+		Values:   requestDetails,
+	})
+
+	// Submit the request.
+	resp, err := http.Post(c.baseURL, "application/json", bytes.NewReader(body))
+	if err != nil {
+		return errors.Wrap(err, errorMessage)
+	}
+	defer resp.Body.Close()
+
+	// Check the HTTP Status code.
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf(errorMessage)
+		}
+		return fmt.Errorf("%s: %s", errorMessage, respBody)
+	}
+
+	return nil
+}
