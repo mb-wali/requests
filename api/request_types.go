42e6389ac66159df3e5624564b5b67b00c0cdfee
Sarah Roberts <sarahr@cyverse.org>
2021-08-12 17:17:49 -0700

    CORE-1453: allow request types to be updated

--- api/request_types.go
+++ api/request_types.go
@@ -124,6 +124,76 @@ func (a *API) RegisterRequestTypeHandler(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, requestType)
 }
 
+// UpdateRequestTypesHandler handles PATCH requests to the /request-types/{name} endpoint.
+func (a *API) UpdateRequestTypesHandler(ctx echo.Context) error {
+	name := ctx.Param("name")
+
+	// Start a transaction.
+	tx, err := a.DB.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	// Get the request type.
+	requestType, err := db.GetRequestType(tx, name)
+	if err != nil {
+		return err
+	}
+	if requestType == nil {
+		return ctx.JSON(http.StatusNotFound, ErrorResponse{
+			Message: fmt.Sprintf("request type %s not found", name),
+		})
+	}
+
+	// Get the maximum number of requests per user for this request type and validate it.
+	maximumRequestsPerUser, err := query.ValidateOptionalIntQueryParam(ctx, "maximum-requests-per-user")
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: err.Error(),
+		})
+	}
+	if maximumRequestsPerUser != nil && *maximumRequestsPerUser <= 0 {
+		return ctx.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: "maximum-requests-per-user must be a positive integer if specified",
+		})
+	}
+
+	// Get the maximum number of concurrent requestws per user for this request type and validate it.
+	maximumConcurrentRequestsPerUser, err := query.ValidateOptionalIntQueryParam(
+		ctx, "maximum-concurrent-requests-per-user",
+	)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: err.Error(),
+		})
+	}
+	if maximumConcurrentRequestsPerUser != nil && *maximumConcurrentRequestsPerUser <= 0 {
+		return ctx.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: "maximum-concurrent-requests-per-user must be a positive integer if specified",
+		})
+	}
+
+	// Simply return the existing object if there were no updates.
+	if maximumRequestsPerUser == nil && maximumConcurrentRequestsPerUser == nil {
+		return ctx.JSON(http.StatusOK, requestType)
+	}
+
+	// Update the request type.
+	requestType, err = db.UpdateRequestType(tx, name, maximumRequestsPerUser, maximumConcurrentRequestsPerUser)
+	if err != nil {
+		return err
+	}
+
+	// Commit the transaction.
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return ctx.JSON(http.StatusOK, requestType)
+}
+
 // GetRequestTypeHandler handles GET requests to the /request-types/{name} endpoint.
 func (a *API) GetRequestTypeHandler(ctx echo.Context) error {
 	name := ctx.Param("name")
